e0e181d934211dda26f0617904d4e8c2d9db8ca9
hoanhatoq <kin.love.205@gmail.com>
2015-02-11 19:02:31 +0700

    jvb_dev_news

--- app/Controller/NewsController.php
+++ app/Controller/NewsController.php
@@ -2,7 +2,6 @@
 
 	App::uses('AppController', 'Controller');
 
-
 	class NewsController extends AppController{
 
 		/*Ten rut gon cua controller*/
@@ -19,6 +18,17 @@
 			$new = $this->News->find('all');
 			$this->set ('new',$new);
 	
+			$lang_code = $this->params->language;
+			if($lang_code == ''){
+				$lang_code = 'jp';
+			}
+
+			$data = $this->News->getAll($lang_code);
+			$this->set('data', $data);
+			/*echo '<pre>';
+			print_r($data);
+			echo '</pre>';*/
+			
 		}
-		
+		    
 	}
--- app/Model/News.php
+++ app/Model/News.php
@@ -4,9 +4,48 @@ App::uses('AppModel', 'Model');
 	class News extends AppModel{
 		
 		/*Khai bao su dung bang*/
+
 		public $useTable= "news_tbl";
 
 		/*truong khoa trinh cua bang*/
+
 		public $primaryKey = "news_id"; 
 
+		public function getAll($lang_code){
+			if($lang_code == 'vi'){
+
+			}
+			
+			$sql = ('
+				select title_content_id,lang_code,value from news_tbl as t1 
+				INNER JOIN expresion_tbl as t2 
+				on t1.title_news_id = t2.title_content_id or t1.content_news_id =t2.title_content_id 
+				WHERE lang_code = "'.$lang_code.'"');
+			/*$options = array(  
+				            
+	        	'joins' => array(
+	                array(
+	                    'table' => 'expresion_tbl',
+	                	'alias' => 'Expresion',
+	                    'type' => 'inner',
+	                    'foreignKey' => false,
+	                    'conditions'=> array(
+	                    	' News.title_news_id = Expresion.title_content_id ',
+
+	                    	'News.content_news_id =  Expresion.title_content_id ',
+	                    	
+	                    ),
+	                ),
+	                   
+	     		),
+	        	'conditions'=> array('Expresion.lang_code = vn')
+			);	*/
+			// $this->recursive = -1;
+			// $coupons = $this->find('all', $options);
+			$coupons = $this->query($sql);
+
+			return $coupons;
+			
+			
+		}
 	}
