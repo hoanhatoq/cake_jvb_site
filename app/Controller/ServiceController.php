ee1e9142fc17b84374bbb4fc036050feb66bf19f
Do Thanh Tung <tung.dothanh@hatoq.com>
2015-02-13 15:16:45 +0700

    update service

--- app/Controller/ServiceController.php
+++ app/Controller/ServiceController.php
@@ -1,6 +1,20 @@
 <?php
 class ServiceController extends AppController {
-		public function index() {
+	public function index($id = null) {
 
-	}
+		$lang_code = $this->params->language;
+		$data = $this->Service->get($lang_code);
+		$this->set('data', $data);
+			if($id == ''){
+				$id = 1;
+			}
+		$detail = $this->Service->getDetail($id,$lang_code);
+		$this->set('detail', $detail);
+	
+
+// echo "<pre>";
+// print_r($id);
+// echo "</pre>";
+// die();
+}
 }
