7e107dbcce71e51cc83fc55da3624230f40b6abd
Ha Manh Linh <hmlinh512@gmail.com>
2015-02-13 17:01:31 +0700

    update page AboutUs

--- app/Controller/AboutUsController.php
+++ app/Controller/AboutUsController.php
@@ -2,22 +2,21 @@
 	App::uses('AppController', 'Controller');
 	class AboutUsController extends AppController{	
 		
-	/**
-	 * Tên rút gọn của controller
-	 */
 		var $name= 'AboutUs';
 
 		public $uses = array('AboutUs');
 
-		public function index(){
-			$data = $this->AboutUs->find('all');
-	       	$this->set("About",$data);
-
-	       	// echo '<pre>';
-	       	// print_r($data);
-	       	// echo '</pre>';
-
+		public function index($id = null){
+			$lang_code = $this->params->language;
+			if($lang_code == ''){
+				$lang_code = 'jp';
+			}
+			$data = $this->AboutUs->getAll($lang_code);
+			$this->set('data', $data);
+				if($id == ''){
+					$id = 1;
+				}
+			$detail = $this->AboutUs->getDetail($id,$lang_code);
+			$this->set('detail', $detail);
 	    }
-		
-		
 	}
